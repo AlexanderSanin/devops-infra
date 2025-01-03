5570c5e7eb8dd4b487bc15e572dc7ba794102602
Oleksandr Yizchak Sanin <alexaaander.sanin@gmail.com>
2025-01-03 09:38:51 +0200

    added tf files to fit for variables, changed gh workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = var.region
 }
 
 terraform {
@@ -13,22 +13,24 @@ terraform {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
+  name = "${var.environment}-vpc"
+  cidr = var.vpc_cidr
 
-  azs             = ["eu-west-1a", "eu-west-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = ["${var.region}a", "${var.region}b"]
+  private_subnets = var.private_subnet_cidrs
+  public_subnets  = var.public_subnet_cidrs
 
   enable_nat_gateway = true
   single_nat_gateway = true
+
+  tags = var.tags
 }
 
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
-  cluster_name    = "counter-service-cluster"
-  cluster_version = "1.27"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
@@ -43,4 +45,6 @@ module "eks" {
       capacity_type  = "ON_DEMAND"
     }
   }
+
+  tags = var.tags
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,50 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "counter-service-cluster"
+}
+
+variable "cluster_version" {
+  description = "EKS cluster version"
+  type        = string
+  default     = "1.27"
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default = {
+    Environment = "production"
+    Terraform   = "true"
+  }
+}
