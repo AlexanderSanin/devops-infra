a73174ec37b3965cb2f55e5a8f0263d80cfc495c
Oleksandr Yizchak Sanin <alexaaander.sanin@gmail.com>
2025-01-03 10:28:29 +0200

    fix: update aws provider version constraints

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"  # Updated to use version 5.x
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
