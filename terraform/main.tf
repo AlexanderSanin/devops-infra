c6f6deb95d8e06e5dac3ea011aedbac79aef74f7
Oleksandr Yizchak Sanin <alexaaander.sanin@gmail.com>
2025-01-05 19:40:55 +0200

    fix: properly declare and configure eks module

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,9 @@ provider "aws" {
   region = var.region
 }
 
-# Remove this duplicate block from main.tf
-# terraform {
-#   backend "s3" {
-#     bucket = "devops-terraform-state-sanin"
-#     key    = "eks/terraform.tfstate"
-#     region = "eu-west-1"
-#   }
-# }
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
 
   name = "${var.environment}-vpc"
   cidr = var.vpc_cidr
@@ -23,6 +15,32 @@ module "vpc" {
 
   enable_nat_gateway = true
   single_nat_gateway = true
+
+  tags = var.tags
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.0.0"  # Specify a version
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
 
   tags = var.tags
 }
